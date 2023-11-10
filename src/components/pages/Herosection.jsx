c533b3a7b07d2d56de9cb2cf45edae9d30ce1055
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-10 15:47:17 +0100

    hero responsive

--- src/components/pages/Herosection.jsx
+++ src/components/pages/Herosection.jsx
@@ -16,32 +16,32 @@ const Herosection = () => {
                         <p className='smallHero mt-lg-4'>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
                     </div>
                     <div className="col-lg-6 col-12 text-center p-5 p-lg-0">
-                        <div id="carouselExampleAutoplaying" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active" data-bs-interval="3000">
-                                    <img src={image1} class="img-fluid p-lg-2 p-4" alt="..."/>
+                        <div id="carouselExampleAutoplaying" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active" data-bs-interval="3000">
+                                    <img src={image1} className="img-fluid p-lg-2 p-4" alt="child image 1"/>
                                 </div>
-                                <div class="carousel-item" data-bs-interval="3000">
-                                <img src={image2} class="img-fluid p-lg-2 p-4" alt="..."/>
+                                <div className="carousel-item" data-bs-interval="3000">
+                                <img src={image2} className="img-fluid p-lg-2 p-4" alt="child image 2"/>
                                 </div>
-                                <div class="carousel-item" data-bs-interval="3000">
-                                    <img src={image3} class="img-fluid p-lg-2 p-4" alt="..."/>
+                                <div className="carousel-item" data-bs-interval="3000">
+                                    <img src={image3} className="img-fluid p-lg-2 p-4" alt="child image 3"/>
                                 </div>
-                                <div class="carousel-item" data-bs-interval="3000">
-                                    <img src={image4} class="img-fluid p-lg-2 p-4" alt="..."/>
+                                <div className="carousel-item" data-bs-interval="3000">
+                                    <img src={image4} className="img-fluid p-lg-2 p-4" alt="child image 4"/>
                                 </div>
-                                <div class="carousel-item" data-bs-interval="3000">
-                                    <img src={image5} class="img-fluid p-lg-2 p-4" alt="..."/>
+                                <div className="carousel-item" data-bs-interval="3000">
+                                    <img src={image5} className="img-fluid p-lg-2 p-4" alt="child image 5"/>
                                 </div>
-                                <div class="carousel-item" data-bs-interval="3000">
-                                    <img src={image6} class="img-fluid p-lg-2 p-4" alt="..."/>
+                                <div className="carousel-item" data-bs-interval="3000">
+                                    <img src={image6} className="img-fluid p-lg-2 p-4" alt="child image 6"/>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-6 col-12 text-center text-lg-start d-lg-none d-md-none" id='bigHero'>
-                        <h1 className='heroText'>Discover a world where education meets excitement</h1>
-                        <p className='smallHero mt-lg-4'>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
+                        <h1 className='heroText heroTextsm'>Discover a world where education meets excitement</h1>
+                        <p className='smallHero smallHerosm mt-lg-4'>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
                     </div>
                 </div>
             </div>
