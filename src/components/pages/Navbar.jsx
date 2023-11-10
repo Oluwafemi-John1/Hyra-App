e4228a8a8a1148ae7bd3c42614bd71c961d36696
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-10 13:55:27 +0100

    nav 2

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -2,30 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    let linkStyle = {
+        color: '#121214',
+        fontFamily: 'Inter',
+        fontSize: '20px',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        lineHeight: 'normal'
+    }
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
+            <nav className="navbar navbar-expand-lg shadow-sm py-lg-1" aria-label="Thirteenth navbar example">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <Link className="navbar-brand col-lg-3 me-0" to="/"><h3 className='ms-2 hyra'>Hyra</h3></Link>
-                    <div className="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
+                    <div className="collapse navbar-collapse d-lg-flex justify-content-lg-between" id="navbarsExample11">
+                        <Link className="navbar-brand col-lg-3 me-0" to="/"><h3 className='ms-lg-5 hyra'>Hyra</h3></Link>
                         <ul className="navbar-nav col-lg-6 justify-content-lg-center">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                <Link className="nav-link active" aria-current="page" to="/" style={linkStyle}>Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/">Our Teachings</Link>
+                                <Link className="nav-link" to="/" style={linkStyle}>Our Teachings</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/">About Us</Link>
+                                <Link className="nav-link" to="/" style={linkStyle}>About Us</Link>
                             </li>
                         </ul>
                         <div className="d-lg-flex ms-lg-5 col-lg-3">
-                            <button className="mx-lg-3 mx-1 signUpBtn px-lg-3">Sign Up</button>
-                            <button className="mx-lg-3 mx-1 logInBtn px-lg-3">Log in</button>
+                            <button className="mx-lg-3 mx-1 signUpBtn px-lg-3 py-lg-2">Sign Up</button>
+                            <button className="mx-lg-3 mx-1 logInBtn px-lg-3 py-lg-2">Log in</button>
                         </div>
                     </div>
                 </div>
