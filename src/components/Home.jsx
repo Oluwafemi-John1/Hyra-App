a9c6dcc9492e32b239a40b5c835c194cfbd3abe9
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-10 14:17:47 +0100

    hero set up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './pages/Navbar'
+import Herosection from './pages/Herosection'
 
 const Home = () => {
   return (
     <>
         <>
             <Navbar/>
+            <Herosection/>
         </>
     </>
   )
---
+++ src/components/pages/Herosection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Herosection = () => {
+  return (
+    <>
+        <div className="container-fluid border border-danger mt-lg-3">
+            <div className="row">
+                <div className="col-lg-6 col-12 border border-info p-lg-5 mx-auto">
+                    <h1 className='heroText'>Discover a world where education meets excitement</h1>
+                </div>
+                <div className="col-lg-6 col-12 border border-warning"></div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Herosection
