db71bc6312b28a2b1c8b376f5d05a73c6c6e73c6
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-10 14:55:08 +0100

    hero section responsive

--- src/components/pages/Herosection.jsx
+++ src/components/pages/Herosection.jsx
@@ -6,12 +6,16 @@ const Herosection = () => {
     <>
         <div className="container-fluid mt-lg-5">
             <div className="row">
-                <div className="col-lg-6 col-12 mx-auto" id='bigHero'>
+                <div className="col-lg-6 col-12 text-center text-lg-start d-none d-lg-block" id='bigHero'>
                     <h1 className='heroText'>Discover a world where education meets excitement</h1>
                     <p className='smallHero mt-lg-4'>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
                 </div>
-                <div className="col-lg-6 col-12 text-center">
+                <div className="col-lg-6 col-12 text-center p-5 p-lg-0">
                     <img src={image1} alt="" loading='lazy' className='img-fluid' />
+                </div>
+                <div className="col-lg-6 col-12 text-center text-lg-start d-lg-none d-md-none" id='bigHero'>
+                    <h1 className='heroText'>Discover a world where education meets excitement</h1>
+                    <p className='smallHero mt-lg-4'>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
                 </div>
             </div>
         </div>
