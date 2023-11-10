42a1915fb7c9b6ac3893aadb8a777402f25084e0
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-10 14:33:13 +0100

    hero section done

--- src/components/pages/Herosection.jsx
+++ src/components/pages/Herosection.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import image1 from '../assets/images/hyra1.png'
 
 const Herosection = () => {
   return (
     <>
-        <div className="container-fluid border border-danger mt-lg-3">
+        <div className="container-fluid mt-lg-5">
             <div className="row">
-                <div className="col-lg-6 col-12 border border-info p-lg-5 mx-auto">
+                <div className="col-lg-6 col-12 mx-auto" id='bigHero'>
                     <h1 className='heroText'>Discover a world where education meets excitement</h1>
+                    <p className='smallHero mt-lg-4'>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
                 </div>
-                <div className="col-lg-6 col-12 border border-warning"></div>
+                <div className="col-lg-6 col-12 text-center">
+                    <img src={image1} alt="" loading='lazy' className='img-fluid' />
+                </div>
             </div>
         </div>
     </>
