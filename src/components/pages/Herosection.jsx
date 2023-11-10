1494d90925b08c024f780ba7bbb93b7082cba21a
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-10 19:32:42 +0100

    fixing carousel autoplay on small device

--- src/components/pages/Herosection.jsx
+++ src/components/pages/Herosection.jsx
@@ -16,24 +16,24 @@ const Herosection = () => {
                         <p className='smallHero mt-lg-4'>Our Islamic app for kids is designed to inspire and teach in the most enjoyable way. Our app is not just educational; it's an adventure in faith</p>
                     </div>
                     <div className="col-lg-6 col-12 text-center p-5 p-lg-0">
-                        <div id="carouselExampleAutoplaying" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                        <div id="carouselExampleAutoplaying" className="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000">
                             <div className="carousel-inner">
-                                <div className="carousel-item active" data-bs-interval="3000">
+                                <div className="carousel-item active">
                                     <img src={image1} className="img-fluid p-lg-2 p-4" alt="child image 1"/>
                                 </div>
-                                <div className="carousel-item" data-bs-interval="3000">
+                                <div className="carousel-item">
                                 <img src={image2} className="img-fluid p-lg-2 p-4" alt="child image 2"/>
                                 </div>
-                                <div className="carousel-item" data-bs-interval="3000">
+                                <div className="carousel-item">
                                     <img src={image3} className="img-fluid p-lg-2 p-4" alt="child image 3"/>
                                 </div>
-                                <div className="carousel-item" data-bs-interval="3000">
+                                <div className="carousel-item">
                                     <img src={image4} className="img-fluid p-lg-2 p-4" alt="child image 4"/>
                                 </div>
-                                <div className="carousel-item" data-bs-interval="3000">
+                                <div className="carousel-item">
                                     <img src={image5} className="img-fluid p-lg-2 p-4" alt="child image 5"/>
                                 </div>
-                                <div className="carousel-item" data-bs-interval="3000">
+                                <div className="carousel-item">
                                     <img src={image6} className="img-fluid p-lg-2 p-4" alt="child image 6"/>
                                 </div>
                             </div>
