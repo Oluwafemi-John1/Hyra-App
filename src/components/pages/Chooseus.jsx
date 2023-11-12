f3bce4f4f7fe2eea48f9de437a80fa586073cd10
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-12 23:45:25 +0100

    images added for section below hero

--- src/components/pages/Chooseus.jsx
+++ src/components/pages/Chooseus.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-
+import img1 from '../assets/images/hyra8.png'
+import img2 from '../assets/images/hyra7.png'
+import img3 from '../assets/images/hyra9.png'
+import img4 from '../assets/images/hyra10.png'
 
 const Chooseus = () => {
     let styleOne = {
@@ -21,21 +24,27 @@ const Chooseus = () => {
         borderRadius: '8px',
         background: '#FFCD4B'
     }
-  return (
-    <>
-        <p className="text-center mt-lg-5" id='chUs'>Why Choose Us</p>
-        <div className="container-fluid border border-danger mx-auto text-center">
-            <div className="row g-3 g-lg-0 g-md-0">
-                <div className="col-lg col-md col-5 border border-info p-3 mx-lg-5 mx-2" style={styleOne}>
-                    <img src="" alt="" className='img-fluid' />
+    return (
+        <>
+            <p className="text-center mt-lg-5" id='chUs'>Why Choose Us</p>
+            <div className="container mx-auto text-center mt-lg-5">
+                <div className="row g-3 g-lg-0 g-md-0">
+                    <div className="col-lg col-md col-5 p-3 mx-lg-5 mx-3" style={styleOne}>
+                        <img src={img1} alt="" className='img-fluid' />
+                    </div>
+                    <div className="col-lg col-md col-5 p-3 mx-lg-5 mx-2" style={styleTwo}>
+                        <img src={img2} alt="" className='img-fluid' />
+                    </div>
+                    <div className="col-lg col-md col-5 p-3 mx-lg-5 mx-3" style={styleThree}>
+                        <img src={img3} alt="" className='img-fluid' />
+                    </div>
+                    <div className="col-lg col-md col-5 p-3 mx-lg-5 mx-2" style={styleFour}>
+                        <img src={img4} alt="" className='img-fluid' />
+                    </div>
                 </div>
-                <div className="col-lg col-md col-5 border border-primary p-3 mx-lg-5 mx-2" style={styleTwo}></div>
-                <div className="col-lg col-md col-5 border border-warning p-3 mx-lg-5 mx-2" style={styleThree}></div>
-                <div className="col-lg col-md col-5 border border-dark p-3 mx-lg-5 mx-2" style={styleFour}></div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Chooseus
