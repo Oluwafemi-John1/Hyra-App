7510c98d8dc7224dd3dbfdbd685935fbb6fe8acb
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-12 23:12:03 +0100

    section below hero starts 2

--- src/components/pages/Chooseus.jsx
+++ src/components/pages/Chooseus.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Chooseus = () => {
   return (
     <>
-        <p className="text-center" id='chUs'>Why Choose Us</p>
+        <p className="text-center mt-lg-5" id='chUs'>Why Choose Us</p>
+        <div className="container-fluid border border-danger">
+            <div className="row">
+                <div className="col-lg border border-info"></div>
+                <div className="col-lg border border-primary"></div>
+                <div className="col-lg border border-warning"></div>
+                <div className="col-lg border border-dark"></div>
+            </div>
+        </div>
     </>
   )
 }
