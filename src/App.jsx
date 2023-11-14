0070cb5b2c2b615e12b78fdccd6c69d666e9304b
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-14 11:06:51 +0100

    sign up in progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Home from './components/Home'
 import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home/>} />
                 <Route path='/signup' element={<Signup/>} />
+                <Route path='/login' element={<Login/>} />
             </Routes>
         </>
     )
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import image1 from '../components/assets/images/hyrasignup.png'
 
 const Signup = () => {
   return (
     <>
-        jfkk
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-lg-6 border border-danger p-2"></div>
+                <div className="col-lg-6 border border-info p-2 d-none d-lg-block">
+                    <div className="text-center py-5">
+                        <img src={image1} alt="sign up image" className='img-fluid' />
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
