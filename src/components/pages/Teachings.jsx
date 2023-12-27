67848b4f0162a16819fa208173488c996e2d4d26
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-27 16:15:33 +0100

    Update Navbar and Teachings components

--- src/components/pages/Teachings.jsx
+++ src/components/pages/Teachings.jsx
@@ -38,6 +38,18 @@ const Teachings = () => {
         padding: '60px 0'
     }
 
+    let btnStyle = {
+        display: 'flex',
+        width: '284px',
+        height: '58px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '16px',
+        flexShrink: '0',
+        borderRadius: '8px',
+        background: '#0802A3'
+    }
+
 
     return (
         <>
@@ -57,6 +69,12 @@ const Teachings = () => {
                     </Link>
                 </div>
             </div>
+            <Link to='' style={btnStyle}>
+                View all
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M23.2251 13.3604L16.0303 20.0891L14.9362 18.9205L20.1376 14.0572L2.07896 13.4505L2.13267 11.8514L20.1913 12.4581L15.3279 7.25825L16.4981 6.16412L23.2251 13.3604Z" fill="white" />
+                </svg>
+            </Link>
         </>
     )
 }
