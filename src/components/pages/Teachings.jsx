fc8212af90a4c8120defc22b1ef34b7dee8ec65c
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-27 14:12:34 +0100

    Update border colors in Teachings component

--- src/components/pages/Teachings.jsx
+++ src/components/pages/Teachings.jsx
@@ -15,13 +15,13 @@ const Teachings = () => {
 
     let styleTwo = {
         borderRadius: '8px',
-        border: '1px solid #0802A3',
+        border: '1px solid #FF7676',
         textDecoration: 'none'
     }
 
     let styleThree = {
         borderRadius: '8px',
-        border: '1px solid #0802A3',
+        border: '1px solid #FF4B91',
         textDecoration: 'none'
     }
     return (
@@ -29,18 +29,16 @@ const Teachings = () => {
             <p className="text-center mt-lg-5" id='chUs'>Our Teachings</p>
             <div className="container mx-auto text-center mt-lg-5">
                 <div className="row g-3 g-lg-0 g-md-0">
-                    <Link className="col-lg col-md col-5 p-3 mx-lg-5 mx-3" style={styleOne}>
-                        <div>
-                            <p className='p-5'>Our app contains the fundamental teaching of Islam for Kids</p>
-                        </div>
+                    <Link className="col-lg col-md col-5 mx-lg-4 mx-2 d-flex justify-content-center align-items-center" style={styleOne}>
+                        <p className='p-5'>Pillars of Islam</p>
                     </Link>
 
-                    <Link className="col-lg col-md col-5 p-3 mx-lg-5 mx-2" style={styleTwo}>
+                    <Link className="col-lg col-md col-5 p-3 mx-lg-4 mx-2 d-flex justify-content-center align-items-center" style={styleTwo}>
                         <div>
                             <p className='p-5'>We make it interesting by including the the Stories of the Prophets and his Companions </p>
                         </div>
                     </Link>
-                    <Link className="col-lg col-md col-5 p-3 mx-lg-5 mx-3" style={styleThree}>
+                    <Link className="col-lg col-md col-5 p-3 mx-lg-4 mx-2 d-flex justify-content-center align-items-center" style={styleThree}>
                         <div>
                             <p className='p-5'>As we all know that Du’a is an essential part of our life as a Muslim. Du’a is also included in our app</p>
                         </div>
