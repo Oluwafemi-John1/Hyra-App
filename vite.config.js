1db10ea17ba1377a7bb5b596e007148282421bc5
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-27 17:39:55 +0100

    Update Vercel and Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://your-api-server',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-    hmr: {
-      overlay: false,
-    },
-    // Add this rewrite rule to handle all routes
-    // This is crucial for client-side routing to work correctly
-    fs: {
-      strict: false,
-    },
-  },
+    plugins: [react()],
+    base: 'https://hyra-app-one.vercel.app/'
 })
