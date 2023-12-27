822f6693b6688135327ed4960304bb0365a15d35
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-27 18:05:56 +0100

    Update styling for Ourteachings component

--- src/components/Ourteachings.jsx
+++ src/components/Ourteachings.jsx
@@ -7,8 +7,8 @@ const Ourteachings = () => {
             <Navbar/>
             <div className="container border text-center border-3 border-info teach">
                 <div className="row mx-auto">
-                    <div className="col-lg col-md col-12 border mx-lg-4 mx-md-2 mx-1">djdj</div>
-                    <div className="col-lg col-md col-12 border mx-lg-4 mx-md-2 mx-1">djdj</div>
+                    <div className="col-lg col-md col-12 border mx-lg-4 mx-md-2 mx-1 p-3 my-3 my-lg-0 my-md-0">djdj</div>
+                    <div className="col-lg col-md col-12 border mx-lg-4 mx-md-2 mx-1 p-3 my-3 my-lg-0 my-md-0">djdj</div>
                 </div>
             </div>
         </>
