646acfe9e53bf183624026b257d13dc7037a7562
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-27 19:15:33 +0100

    Add Getstarted component and update App.css

---
+++ src/components/Getstarted.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Getstarted = () => {
+    return (
+        <>
+            <div className='container-fluid'>
+                <div className="row">
+                    <div className="col-6"></div>
+                    <div className="col-6"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Getstarted
