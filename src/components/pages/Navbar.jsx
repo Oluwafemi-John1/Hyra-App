2ebd340b4fc9dd1ae3ae647a4f5b8bd726bbeca8
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-27 13:13:56 +0100

    Refactor Navbar component

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import StickyHeader from 'react-sticky-header';
-import Home from '../Home';
 
 
 const Navbar = () => {
@@ -26,33 +25,32 @@ const Navbar = () => {
         <>
             <StickyHeader header={
                 <>
-                <nav className="navbar navbar-expand-lg shadow-sm py-lg-1" id='nav' aria-label="Thirteenth navbar example">
-                    <div className="container-fluid">
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
+                    <nav className="navbar navbar-expand-lg shadow-sm py-lg-1" id='nav' aria-label="Thirteenth navbar example">
+                        <div className="container-fluid">
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
 
-                        <div className="collapse navbar-collapse d-lg-flex justify-content-lg-between" id="navbarsExample11">
-                            <Link className="navbar-brand col-lg-3 me-0" to="/"><h3 className='ms-lg-5 hyra'>Hyra</h3></Link>
-                            <ul className="navbar-nav col-lg-6 justify-content-lg-center">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/" style={linkStyle}>Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/" style={linkStyle}>Our Teachings</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/" style={linkStyle}>About Us</Link>
-                                </li>
-                            </ul>
-                            <div className="d-lg-flex ms-lg-5 col-lg-3">
-                                <button className="mx-lg-3 mx-1 signUpBtn px-lg-3 py-lg-2" onClick={signUp}>Sign Up</button>
-                                <button className="mx-lg-3 mx-1 logInBtn px-lg-3 py-lg-2" onClick={logIn}>Log in</button>
+                            <div className="collapse navbar-collapse d-lg-flex justify-content-lg-between" id="navbarsExample11">
+                                <Link className="navbar-brand col-lg-3 me-0" to="/"><h3 className='ms-lg-5 hyra'>Hyra</h3></Link>
+                                <ul className="navbar-nav col-lg-6 justify-content-lg-center">
+                                    <li className="nav-item">
+                                        <Link className="nav-link active" aria-current="page" to="/" style={linkStyle}>Home</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/" style={linkStyle}>Our Teachings</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/" style={linkStyle}>About Us</Link>
+                                    </li>
+                                </ul>
+                                <div className="d-lg-flex ms-lg-5 col-lg-3">
+                                    <button className="mx-lg-3 mx-1 signUpBtn px-lg-3 py-lg-2" onClick={signUp}>Sign Up</button>
+                                    <button className="mx-lg-3 mx-1 logInBtn px-lg-3 py-lg-2" onClick={logIn}>Log in</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </nav>
-                <Home/>
+                    </nav>
                 </>
             } />
         </>
