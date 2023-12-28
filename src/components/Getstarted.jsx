bf143d93f37df66f28baa4bd686a96652205a29a
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-28 08:13:27 +0100

    Add padding to Getstarted component

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -8,14 +8,16 @@ const Getstarted = () => {
         fontSize: '32px',
         fontStyle: 'normal',
         fontWeight: '500',
-        lineHeight: '48px'
+        lineHeight: '48px',
+        padding: '5px 12%'
     }
+
     return (
         <>
             <div className='container-fluid get-started-with-us'>
                 <div className="row">
                     <div className="col-6 p-5">
-                        <p style={paraStyle}>The Prophet (S.A.W) said “Seeking knowledge is an obligation upon every Muslim”</p>
+                        <p style={paraStyle} className='paraGetStarted'>The Prophet (S.A.W) said “Seeking knowledge is an obligation upon every Muslim”</p>
                     </div>
                     <div className="col-6 py-3 text-end pe-5">
                         <img src={simage} alt="" className="img-fluid" />
