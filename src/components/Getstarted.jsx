0dedf636194a7e990f71be1577bb15f1b1799e41
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-28 09:06:38 +0100

    Add Link component to Getstarted.jsx

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import simage from '../components/assets/images/simage.png'
+import { Link } from 'react-router-dom'
 
 const Getstarted = () => {
     let paraStyle = {
@@ -11,13 +12,18 @@ const Getstarted = () => {
         lineHeight: '48px',
     }
 
+    let linkStyle = {
+        borderRadius: '5.821px',
+        background: '#FFF'
+    }
+
     return (
         <>
             <div className='container-fluid get-started-with-us mt-lg-5'>
                 <div className="row">
                     <div className="col-6 p-lg-5 p-md-3 p-1">
                         <p style={paraStyle} className='paraGetStarted'>The Prophet (S.A.W) said “Seeking knowledge is an obligation upon every Muslim”</p>
-                        
+                        <Link></Link>
                     </div>
                     <div className="col-6 py-3 text-end pe-5">
                         <img src={simage} alt="" className="img-fluid" />
