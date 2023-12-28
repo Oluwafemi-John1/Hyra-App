eeeff6e3fed672f3e8d738e01488c574887a8fff
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-28 09:31:25 +0100

    Update link styles in Teachings component

--- src/components/pages/Teachings.jsx
+++ src/components/pages/Teachings.jsx
@@ -58,15 +58,15 @@ const Teachings = () => {
             <p className="text-center mt-lg-5 mt-3" id='chUs'>Our Teachings</p>
             <div className="container mx-auto text-center mt-lg-4">
                 <div className="row g-3 g-lg-0 g-md-0">
-                    <Link className="col-lg col-md col-11 mx-lg-4 mx-2 d-flex justify-content-center align-items-center" style={styleOne}>
+                    <Link className="col-lg col-md col mx-lg-4 mx-2 d-flex justify-content-center align-items-center" style={styleOne}>
                         <p className='p-5'>Pillars of Islam</p>
                     </Link>
 
-                    <Link className="col-lg col-md col-11 mx-lg-4 mx-2 d-flex justify-content-center align-items-center" style={styleTwo}>
+                    <Link className="col-lg col-md col mx-lg-4 mx-2 d-flex justify-content-center align-items-center" style={styleTwo}>
                         <p className='p-5'>Articles of Faith</p>
                     </Link>
 
-                    <Link className="col-lg col-md-6 mt-md-4 mt-lg-0 col-11 mx-lg-4 mx-2 d-flex justify-content-center align-items-center mx-md-auto" style={styleThree}>
+                    <Link className="col-lg col-md-6 mt-md-4 mt-lg-0 col mx-lg-4 mx-2 d-flex justify-content-center align-items-center mx-md-auto" style={styleThree}>
                         <p className='p-5'>Stories of the Prophets</p>
                     </Link>
                 </div>
