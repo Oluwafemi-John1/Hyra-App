326e1f24013b8ec3474096e25e7fd49ba8c060ab
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-28 07:52:03 +0100

    Add styling to Getstarted component

--- src/components/Getstarted.jsx
+++ src/components/Getstarted.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import simage from '../components/assets/images/simage.png'
 
 const Getstarted = () => {
+    let paraStyle = {
+        color: '#FFF',
+        fontFamily: 'Inter',
+        fontSize: '32px',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        lineHeight: '48px'
+    }
     return (
         <>
             <div className='container-fluid get-started-with-us'>
                 <div className="row">
-                    <div className="col-6"></div>
-                    <div className="col-6 py-3">
+                    <div className="col-6 p-5">
+                        <p style={paraStyle}>The Prophet (S.A.W) said “Seeking knowledge is an obligation upon every Muslim”</p>
+                    </div>
+                    <div className="col-6 py-3 text-end pe-5">
                         <img src={simage} alt="" className="img-fluid" />
                     </div>
                 </div>
