e0a7d310e86c44108d7d1a47edc4047ad7954092
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-31 17:11:10 +0100

    Add content to Aboutus component

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -5,9 +5,12 @@ const Aboutus = () => {
   return (
     <>
         <Navbar/>
-        <div className="container-fluid">
+        <div className="container teach">
             <div className="row">
-                <div className="col-lg-6 col-md-6 col-12"></div>
+                <div className="col-lg-6 col-md-6 col-12">
+                    <h3>Hyra Islamic Stories for Kids</h3>
+                    <p>Hyra Islamic Stories for Kids is an engaging and interactive Educational app designed to introduce Islamic teachings to children.</p>
+                </div>
                 <div className="col-lg-6 col-md-6 col-12"></div>
             </div>
         </div>
