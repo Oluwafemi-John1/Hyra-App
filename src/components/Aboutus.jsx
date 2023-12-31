390d87feb4ffba89985e6c07c362e131d17dc3f5
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-31 17:22:22 +0100

    Add image to About Us page

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './pages/Navbar'
+import pic1 from '../components/assets/images/aboutus1.png'
 
 const Aboutus = () => {
   return (
@@ -11,7 +12,9 @@ const Aboutus = () => {
                     <h3>Hyra Islamic Stories for Kids</h3>
                     <p>Hyra Islamic Stories for Kids is an engaging and interactive Educational app designed to introduce Islamic teachings to children.</p>
                 </div>
-                <div className="col-lg-6 col-md-6 col-12"></div>
+                <div className="col-lg-6 col-md-6 col-12">
+                    <img src={pic1} className='img-fluid' alt="" />
+                </div>
             </div>
         </div>
     </>
